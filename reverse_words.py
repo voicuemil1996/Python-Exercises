90f413a3946e6426352ec5bfd62529e84a126c94
voicu emil <voicuemil1996@gmail.com>
2023-04-28 14:53:53 +0300

    adding exercise

---
+++ reverse_words.py
@@ -0,0 +1,26 @@
+"""
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more
+letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be included only when more
+than one word is present.
+
+Examples:
+“Hey fellow warriors” //returns “Hey wollef sroirraw"
+"This is a test" //returns "This is a test"
+"This is another test" //returns "This is rehtona test"
+"""
+
+
+def reverse_words(input_string):
+
+    words_list = input_string.split()
+    for word_index, word in enumerate(words_list):
+        if len(word) > 5:
+            words_list[word_index] = word[::-1]
+
+    return ' '.join(words_list)
+
+
+assert reverse_words("Hey fellow warriors") == "Hey wollef sroirraw"
+assert reverse_words("This is a test") == "This is a test"
+assert reverse_words("This is another test") == "This is rehtona test"
+assert reverse_words("This") == "This"
